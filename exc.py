2aba58679bebfdd5bc44c2c456309d422f3193cc
Tony Narlock <tony@git-pull.com>
2018-01-11 10:14:10 -0600

    initial RemoteCustomerNotFound

--- exc.py
+++ exc.py
@@ -12,6 +12,11 @@ processor's copy of data.
 Try/catch in Python is powerful. You can use a parent class of the exception,
 such as SAASKitException, as a catch-all exception for more specific situations.
 
+Another benefit of this is SAASKitException supports wrapping the original
+exception, should one exist. In payment processor exceptions, exceptions also
+can come from requests (a dependency used when stripe library is making API
+calls).
+
 Suggestion: Examine setup_membership()'s default behavior for handling cases.
 
 Other Idea: Have a command to create plans on the payment processor side.
@@ -21,4 +26,8 @@ Other Idea: Have a command to create plans on the payment processor side.
 class SAASKitException(Exception):
 
     """Base exception for execeptions emitted from SAASKit."""
+    pass
+
+
+class RemoteCustomerNotFound(SAASKitException):
     pass
