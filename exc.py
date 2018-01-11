d60aa4de345219288b17d30416c9f44248b681db
Tony Narlock <tony@git-pull.com>
2018-01-11 09:45:05 -0600

    add some more exception documentation

--- exc.py
+++ exc.py
@@ -7,7 +7,14 @@ to denote an error propagated from library code.
 In some instances, SAASKitError may also propagate contextual information to
 allow you to recover. In the instance of _setup_membership, errors propagate
 depending on local data being out of synchronization with the payment
-processor's
+processor's copy of data.
+
+Try/catch in Python is powerful. You can use a parent class of the exception,
+such as SAASKitException, as a catch-all exception for more specific situations.
+
+Suggestion: Examine setup_membership()'s default behavior for handling cases.
+
+Other Idea: Have a command to create plans on the payment processor side.
 """
 
 
