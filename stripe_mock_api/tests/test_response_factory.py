f335404f530b5565704f657227db94d97b12c1ae
Tony Narlock <tony@git-pull.com>
2018-01-22 12:16:38 -0600

    add test for add_response

--- stripe_mock_api/tests/test_response_factory.py
+++ stripe_mock_api/tests/test_response_factory.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from ..factory import add_response, StripeMockAPI
+import responses
+import stripe
+
+from ..factory import StripeMockAPI, add_response, fake_customer
+
+
+@responses.activate
+def test_add_response():
+    customer_id = 'cus_hihi'
+    add_response(
+        'GET',
+        '{}/v1/customers/{}'.format(stripe.api_base, customer_id),
+        fake_customer(customer_id),
+        200,
+    )
+
+    customer = stripe.Customer.retrieve(customer_id)
+    assert customer.id == customer_id
