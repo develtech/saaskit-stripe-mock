8ba4536a0b6687b09a8eb90d5d40c720c917efc4
Tony Narlock <tony@git-pull.com>
2018-01-22 17:31:30 -0600

    test fake_empty_sources and fake_sources

--- stripe_mock/tests/test_fake.py
+++ stripe_mock/tests/test_fake.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from ..fake import fake_customer
+from ..fake import fake_customer, fake_empty_sources, fake_source
 
 
 def test_fake_customer():
     customer_id = 'cus_ok'
     customer = fake_customer(customer_id)
     assert customer['id'] == customer_id
+
+
+def test_fake_source():
+    customer_id = 'cus_ok'
+    source = fake_source(customer_id)
+    assert source['customer'] == customer_id
+
+
+def test_fake_empty_sources():
+    customer_id = 'cus_ok'
+    source = fake_empty_sources(customer_id)
+    assert customer_id in source['url']
