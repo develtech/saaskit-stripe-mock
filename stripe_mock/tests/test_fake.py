6049f5cd48db085ed1bf1625ec2d88a7a8122fb0
Tony Narlock <tony@git-pull.com>
2018-01-23 14:05:29 -0600

    add test for fake_plan

--- stripe_mock/tests/test_fake.py
+++ stripe_mock/tests/test_fake.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from ..fake import fake_customer, fake_empty_sources, fake_source
+from ..fake import fake_customer, fake_empty_sources, fake_source, fake_plan
 
 
 def test_fake_customer():
@@ -13,6 +13,12 @@ def test_fake_source():
     customer_id = 'cus_ok'
     source = fake_source(customer_id)
     assert source['customer'] == customer_id
+
+
+def test_fake_plan():
+    plan_id = 'test_plan'
+    plan = fake_plan(plan_id)
+    assert plan['id'] == plan_id
 
 
 def test_fake_empty_sources():
