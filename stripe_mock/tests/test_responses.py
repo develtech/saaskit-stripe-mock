440c84483341ac5c849946bd8653b3155821b0c1
Tony Narlock <tony@git-pull.com>
2018-01-23 23:35:51 -0600

    rename to test_mock_api, split add_response to test_response

---
+++ stripe_mock/tests/test_responses.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import responses
+import stripe
+
+from ..factory import add_response
+from ..fake import fake_customer
+
+
+@responses.activate
+def test_add_response():
+    customer_id = 'cus_hihi'
+    add_response(
+        'GET',
+        '{}/v1/customers/{}'.format(stripe.api_base, customer_id),
+        fake_customer(customer_id),
+        200,
+    )
+
+    customer = stripe.Customer.retrieve(customer_id)
+    assert customer.id == customer_id
