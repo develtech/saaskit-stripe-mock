0dc5dcaef22d63a3d69484e58eef6aae833afc30
Tony Narlock <tony@git-pull.com>
2018-01-23 23:39:15 -0600

    add test for add_callback

--- stripe_mock/tests/test_responses.py
+++ stripe_mock/tests/test_responses.py
@@ -2,7 +2,7 @@
 import responses
 import stripe
 
-from ..factory import add_response
+from ..factory import add_response, add_callback
 from ..fake import fake_customer
 
 
@@ -14,6 +14,19 @@ def test_add_response():
         '{}/v1/customers/{}'.format(stripe.api_base, customer_id),
         fake_customer(customer_id),
         200,
+    )
+
+    customer = stripe.Customer.retrieve(customer_id)
+    assert customer.id == customer_id
+
+
+@responses.activate
+def test_add_callback():
+    customer_id = 'cus_hihi'
+    add_callback(
+        'GET',
+        '{}/v1/customers/{}'.format(stripe.api_base, customer_id),
+        lambda request: (200, {}, fake_customer(customer_id)),
     )
 
     customer = stripe.Customer.retrieve(customer_id)
