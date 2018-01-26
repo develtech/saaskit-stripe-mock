6428aafff9207076f7537e71174e02b2f3f4caf8
Tony Narlock <tony@git-pull.com>
2018-01-26 17:59:25 -0600

    consolidate url patterns into string templates

--- stripe_mock/patterns.py
+++ stripe_mock/patterns.py
@@ -11,11 +11,24 @@ CUSTOMER_SOURCE_OBJECT_URL_RE = re.compile(
 CUSTOMER_SOURCE_LIST_URL_RE = re.compile(
     r'{}/(\w+)/sources(\?object=(\w+)?)?'.format(CUSTOMER_URL_BASE)
 )
+CUSTOMER_SOURCE_OBJECT_URL_TPL = """
+{customer_url_base}/{customer_id}/sources/{source_id}
+""".strip()
 SOURCE_URL_BASE = '{}/v1/sources'.format(stripe.api_base)
 SOURCE_URL_RE = re.compile(r'{}/(\w+)'.format(SOURCE_URL_BASE))
 PLAN_URL_BASE = '{}/v1/plans'.format(stripe.api_base)
 PLAN_URL_RE = re.compile(r'{}/(\w+)'.format(PLAN_URL_BASE))
 SUBSCRIPTION_URL_BASE = '{}/v1/subscriptions'.format(stripe.api_base)
+SUBSCRIPTION_OBJECT_URL_TPL = '{}/{{subscription_id}}'.format(SUBSCRIPTION_URL_BASE)
 SUBSCRIPTION_URL_RE = re.compile(r'{}/(\w+)'.format(SUBSCRIPTION_URL_BASE))
+CUSTOMER_SUBSCRIPTION_OBJECT_URL_RE = re.compile(
+    r'{}/(\w+)/subscriptions/(\w+)'.format(SUBSCRIPTION_URL_BASE)
+)
+CUSTOMER_SUBSCRIPTION_LIST_URL_RE = re.compile(
+    r'{}/(\w+)/subscriptions(\?limit=(\w+)?)?'.format(CUSTOMER_URL_BASE)
+)
+CUSTOMER_SUBSCRIPTION_LIST_URL_TPL = """
+{customer_url_base}/{customer_id}/subscriptions
+""".strip()
 COUPON_URL_BASE = '{}/v1/coupons'.format(stripe.api_base)
 COUPON_URL_RE = re.compile(r'{}/(\w+)'.format(COUPON_URL_BASE))
